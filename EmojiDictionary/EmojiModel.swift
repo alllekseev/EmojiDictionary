9041a61f825f3ba8617b7c1c9b006605f1366d9f
Alekseev Oleg <01.wftp@gmail.com>
2023-04-14 15:29:06 +0300

    Initial Commit

---
+++ EmojiDictionary/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiDictionary
+//
+//  Created by Олег Алексеев on 13.04.2023.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
