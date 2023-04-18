60b210b0f94ec09a3e7521f02b78c37b298d155e
Alekseev Oleg <01.wftp@gmail.com>
2023-04-18 18:48:24 +0300

    Add persisting data

--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -9,88 +9,17 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
     
-    var emojis: [Emoji] = [
-       Emoji(
-        symbol: "😀",
-        name: "Grinning Face",
-        description: "A typical smiley face.",
-        usage: "happiness"
-       ),
-       Emoji(
-        symbol: "😕",
-        name: "Confused Face",
-        description: "A confused, puzzled face.",
-        usage: "unsure what to think; displeasure"
-       ),
-       Emoji(
-        symbol: "😍",
-        name: "Heart Eyes",
-        description: "A smiley face with hearts for eyes.",
-        usage: "love of something; attractive"),
-       Emoji(
-        symbol: "🧑‍💻",
-        name: "Developer",
-        description: "A person working on a MacBook (probably using Xcode to write iOS apps in Swift).",
-        usage: "apps, software, programming"
-       ),
-       Emoji(
-        symbol: "🐢",
-        name: "Turtle",
-        description: "A cute turtle.",
-        usage: "something slow"
-       ),
-       Emoji(
-        symbol: "🐘",
-        name: "Elephant",
-        description: "A gray elephant.",
-        usage: "good memory"
-       ),
-       Emoji(
-        symbol: "🍝",
-        name: "Spaghetti",
-        description: "A plate of spaghetti.",
-        usage: "spaghetti"
-       ),
-       Emoji(
-        symbol: "🎲",
-        name: "Die",
-        description: "A single die.",
-        usage: "taking a risk, chance; game"
-       ),
-       Emoji(
-        symbol: "⛺️",
-        name: "Tent",
-        description: "A small tent.",
-        usage: "camping"
-       ),
-       Emoji(
-        symbol: "📚",
-        name: "Stack of Books",
-        description: "Three colored books stacked on each other.",
-        usage: "homework, studying"
-       ),
-       Emoji(
-        symbol: "💔",
-        name: "Broken Heart",
-        description: "A red, broken heart.",
-        usage: "extreme sadness"
-       ),
-       Emoji(
-        symbol: "💤",
-        name: "Snore",
-        description: "Three blue \'z\'s.",
-        usage: "tired, sleepiness"
-       ),
-       Emoji(
-        symbol: "🏁",
-        name: "Checkered Flag",
-        description: "A black-and-white checkered flag.",
-        usage: "completion"
-       )
-    ]
+    var emojis: [Emoji] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let savedEmojis = Emoji.loadFromFile() {
+            emojis = savedEmojis
+        } else {
+            emojis = Emoji.sampleEmojis
+        }
+        
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 44.0
     }
@@ -120,14 +49,6 @@ class EmojiTableViewController: UITableViewController {
         return .delete
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -136,8 +57,6 @@ class EmojiTableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
-    
-
     
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
@@ -172,6 +91,8 @@ class EmojiTableViewController: UITableViewController {
             emojis.append(emoji)
             tableView.insertRows(at: [newIndexPath], with: .automatic)
         }
+        
+        Emoji.saveToFile(emojis: emojis)
     }
     
     
